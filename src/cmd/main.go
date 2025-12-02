1f3fabc26cf3d3d3ede2ec1e91d726e178eaab97
alirzamehrzad <alireza.me@hotmail.com>
2025-12-02 15:38:39 +0330

    implement postgres using gorm & some bugs fixed in redis configurations

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/alirzamehrzad/drivey/api"
 	"github.com/alirzamehrzad/drivey/config"
 	"github.com/alirzamehrzad/drivey/data/cache"
+	"github.com/alirzamehrzad/drivey/data/db"
 )
 
 func main() {
 	cfg := config.GetConfig()
-	cache.InitRedis(cfg)
+
+	err := cache.InitRedis(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer cache.CloseRedis()
+
+	err = db.InitDb(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.CloseDb()
 
 	api.InitServer(cfg)
 }
---
+++ src/data/db/postgres.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/alirzamehrzad/drivey/config"
+)
+
+var dbClient *gorm.DB
+
+func InitDb(cfg *config.Config) error {
+	cnn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Tehran",
+		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User,
+		cfg.Postgres.Password, cfg.Postgres.DbName, cfg.Postgres.SSLMode,
+	)
+	dbClient, err := gorm.Open(postgres.Open(cnn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	sqlDb, _ := dbClient.DB()
+	err = sqlDb.Ping()
+	if err != nil {
+		return err
+	}
+
+	sqlDb.SetMaxIdleConns(cfg.Postgres.MaxIdleConns)
+	sqlDb.SetMaxOpenConns(cfg.Postgres.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(cfg.Postgres.ConnMaxLifetime * time.Minute)
+
+	log.Println("Db connection established")
+
+	return nil
+}
+
+func GertDb() *gorm.DB {
+	return dbClient
+}
+
+func CloseDb() {
+	con, _ := dbClient.DB()
+	con.Close()
+}
